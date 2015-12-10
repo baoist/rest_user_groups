4c673f6389fac41c5bb33d9602f467593a9149ac
Brad Olson <iam@brad.io>
2015-12-09 17:52:10 -0800

    run curl examples. remove ref readme

---
+++ run_tests.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+generate_string()
+{
+  cat /dev/urandom | env LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1
+}
+
+pause()
+{
+  read -rsp $'\nPress any key to continue...\n' -n1 key
+}
+
+new_userid=$(generate_string)
+new_groupid=$(generate_string)
+
+# POST requests
+printf "\nPOST\n\n"
+
+printf "/users:\n%s\n" \
+  "$(curl -sb -H "Accept: application/json" -H "Content-type: application/json" -X POST -d ' {"first_name": "fname", "last_name": "lname", "userid": "'"$new_userid"'"}' $(docker-machine ip usergroups)/users)"
+
+printf "/groups/<groupid>:\n%s\n" \
+  "$(curl -sb -H "Accept: application/json" -H "Content-type: application/json" -X POST $(docker-machine ip usergroups)/groups/$new_groupid)"
+
+pause
+
+# PUT requests
+printf "\nPUT\n\n"
+
+printf "/users/<userid>:\n%s\n" \
+  "$(curl -sb -H "Accept: application/json" -H "Content-type: application/json" -X PUT -d ' {"first_name": "NewFirstName", "last_name": "NewLastName", "userid": "'"$new_userid"'"}' $(docker-machine ip usergroups)/users/$new_userid)"
+
+printf "/groups/<groupid>:\n%s\n" \
+  "$(curl -sb -H "Accept: application/json" -H "Content-type: application/json" -X PUT -d '{"users": ["'$new_userid'"]}' $(docker-machine ip usergroups)/groups/$new_groupid)"
+
+pause
+
+# GET requests
+printf "\nGET\n\n"
+
+printf "/users:\n%s\n" \
+  "$(curl -sb -H "Accept: application/json" "$(docker-machine ip usergroups)/users")"
+
+printf "/users/<userid>:\n%s\n" \
+  "$(curl -sb -H "Accept: application/json" "$(docker-machine ip usergroups)/users/$new_userid")"
+
+printf "/groups/<groupid>:\n%s\n" \
+  "$(curl -sb -H "Accept: application/json" "$(docker-machine ip usergroups)/groups/$new_groupid")"
+
+pause
+
+# DELETE requests
+printf "\nDELETE\n\n"
+
+printf "/groups/<groupid>:\n%s\n" \
+  "$(curl -sb -H "Accept: application/json" -H "Content-type: application/json" -X DELETE $(docker-machine ip usergroups)/groups/$new_groupid)"
+
+printf "/users/<userid>:\n%s\n" \
+  "$(curl -sb -H "Accept: application/json" -H "Content-type: application/json" -X DELETE $(docker-machine ip usergroups)/users/$new_userid)"
